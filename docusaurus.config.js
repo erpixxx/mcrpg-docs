4c6f947c8b1a1deb7961be117a75f9cc3fce2763
Matou0014 <yi14clan@gmail.com>
2021-08-18 14:45:46 -0400

    Retry

---
+++ docusaurus.config.js
@@ -0,0 +1,52 @@
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+module.exports = {
+  title: 'RPG Docs',
+  tagline: 'Documentation for Exvoid RPG.',
+  url: 'https://mcrpg-docs.exvoid.net',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'static/img/ExvoidNetworkPrefix.svg',
+  organizationName: 'ExvoidNet', // Usually your GitHub org/user name.
+  projectName: 'mcrpg-docs', // Usually your repo name.
+  themeConfig: {
+    colorMode: {
+		defaultMode: 'dark',
+		},
+    navbar: {
+      title: 'RPG Docs',
+      logo: {
+        alt: 'Logo',
+        src: 'static/img/ExvoidNetworkPrefix.png',
+      },
+      items: [],
+    },
+    footer: {
+      style: 'dark',
+      links: [],
+      copyright: `Copyright © ${new Date().getFullYear()} Exvoid Network.`,
+    },
+    prism: {
+      theme: lightCodeTheme,
+      darkTheme: darkCodeTheme,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+			routeBasePath: '/',
+			sidebarPath: require.resolve('./sidebars.js'),
+			editUrl: 'https://github.com/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
